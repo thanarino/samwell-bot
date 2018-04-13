a91c35d0d5537e5266b65cadca61cb0834775fd4
Jonathan Arino <arinojonathan@gmail.com>
2018-04-13 22:31:53 +0800

    added more fields for student object

--- index.js
+++ index.js
@@ -54,29 +54,6 @@ function processPostback(event) {
     var payload = event.postback.payload;
 
     if (payload === "Greeting") {
-        // Get user's first name from the User Profile API
-        // and include it in the greeting
-        // request({
-        //     url: "https://graph.facebook.com/v2.6/" + senderId,
-        //     qs: {
-        //         access_token: process.env.PAGE_ACCESS_TOKEN,
-        //         fields: "first_name"
-        //     },
-        //     method: "GET"
-        // }, function (error, response, body) {
-        //     var greeting = "";
-        //     if (error) {
-        //         console.log("Error getting user's name: " + error);
-        //     } else {
-        //         var bodyObj = JSON.parse(body);
-        //         name = bodyObj.first_name;
-        //         greeting = "Hi " + name + ". ";
-        //     }
-        //     var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
-        //     sendMessage(senderId, {
-        //         text: message
-        //     });
-        // });
         checkID(senderId);
     }
 }
@@ -148,6 +125,8 @@ function checkID(userID) {
                                 "family_name": bodyObj.last_name,
                                 "profile_pic": bodyObj.profile_pic,
                                 "gender": bodyObj.gender,
+                                "createdAt": new Date(),
+                                "roles": ['student'],
                             };
 
                             Student.create(data, function (err, results) {
