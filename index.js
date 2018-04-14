cd816ea51a8ddea18f6d681b234272f1f02941c0
Jonathan Arino <arinojonathan@gmail.com>
2018-04-14 14:00:43 +0800

    add wit

--- index.js
+++ index.js
@@ -2,9 +2,36 @@ var express = require("express");
 var request = require("request-promise");
 var bodyParser = require("body-parser");
 var mongoose = require('mongoose');
+var Wit = require('node-wit');
+var log = require('node-wit');
 
 var db = mongoose.connect(process.env.MONGODB_URI);
 var Student = require('./models/students');
+
+const WIT_TOKEN = process.env.WIT_TOKEN;
+const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+const FB_APP_SECRET = process.env.FB_APP_SECRET;
+
+const sessions = {};
+
+const findOrCreateSession = (fbid) => {
+    let sessionId;
+    Object.keys(sessions).forEach(k => {
+        if (sessions[k].fbid === fbid) {
+            sessionId = k;
+        }
+    });
+    if (!sessionId) {
+        sessionId = new Date().toISOString();
+        sessions[sessionId] = { fbid: fbid, context: {} };
+    }
+    return sessionId;
+};
+
+const wit = new Wit({
+    accessToken: WIT_TOKEN,
+    logger: new log.Logger(log.INFO)
+});
 
 var app = express();
 app.use(bodyParser.urlencoded({
@@ -41,6 +68,24 @@ app.post("/webhook", function (req, res) {
             entry.messaging.forEach(function (event) {
                 if (event.postback) {
                     processPostback(event);
+                } else if (event.message && !event.message.is_echo) {
+                    const sender = event.sender.id;
+                    const { text, attachments } = event.message;
+
+                    if (attachments) {
+                        sendMessage(sender, { text: 'Sorry, I can only understand text messages for now.' })
+                            .catch(console.error);
+                    } else if (text) {
+                        wit.message(text).then((entities) => {
+                            console.log(entities);
+                            sendMessage(sender, { text: `We've recieved your message: ${text}.` })
+                        })
+                            .catch((err) => {
+                                console.log('Oops, we got an error from Wit: ', err.stack || err);
+                        }) 
+                    } else {
+                        console.log('recieved event', JSON.stringify(event));
+                    }
                 }
             });
         });
