ca1eb39ae3e187aac3bff834bd00f91414d21aad
Jonathan Arino <arinojonathan@gmail.com>
2018-04-15 18:33:34 +0800

    recast.ai integration 13

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ analyzeEntities = (sender, res, input) => {
             }
         } else if (res.intents[0].slug === "addclass") {
             client.request.converseText(input, { conversationToken: sender }).then((res) => {
-                console.log(res.reply());
+                console.log(res);
                 sendMessage(sender, { text: res.reply() });
             })
         }
