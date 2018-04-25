bd5c96c8adc392f35127afabf618fd5c24a90cb0
Jonathan Arino <arinojonathan@gmail.com>
2018-04-25 23:20:00 +0800

    bug fixerino 25

--- index.js
+++ index.js
@@ -506,73 +506,99 @@ app.post("/verify-consultation-hours", (req, res) => {
     received.conversation.memory.teacher.consultationHours.map((day) => {
         console.log(day);
         if (day.fullName === weekday) {
-            day.time.map((time) => {
-                console.log(time);
-                let tripcheck = checkConsultationHoursConflict(time, u_start, u_end, t_id);
-                if (_.includes(tripcheck, true)) {
-                    if (_.includes(checkConsultationConflict(u_start, u_end, t_id), true)) {
-                        //schedule here
+            if (day.time.length === 0) {
+                let toSend = Object.assign({}, {
+                    replies: [
+                        {
+                            type: 'text',
+                            content: 'There doesn\'t seem to be scheduled consultation hours for this day. Try scheduling for another date.'
+                        }
+                    ],
+                }, { conversation: { memory: {} } });
+                Conversationid.update({ conversationid: received.conversation.id }, { $set: { conversationid: undefined } });
+                res.send(toSend);
+            } else {
+                day.time.map((time) => {
+                    console.log(time);
+                    let tripcheck = checkConsultationHoursConflict(time, u_start, u_end, t_id);
+                    if (_.includes(tripcheck, true)) {
+                        if (_.includes(checkConsultationConflict(u_start, u_end, t_id), true)) {
+                            //schedule here
 
-                        Conversationid.findOne({ conversationid: received.conversation.id }, function (err, obj) {
-                            if (obj) {
-                                Consultations.create({
-                                    userID: obj.fbid,
-                                    studentID: obj.fbid,
-                                    teacherID: t_id,
-                                    startTime: u_start.format('HH:mm'),
-                                    endTime: u_end.format('HH:mm'),
-                                    date: u_start.dayOfYear(),
-                                    year: u_start.get('year'),
-                                    isDone: false,
-                                    isDeleted: false,
-                                    isApprovedByStudent: true,
-                                    isApprovedByTeacher: false,
-                                    createdAt: new Date()
-                                }, function (err, res) {
-                                    if (err) {
-                                        let toSend = Object.assign({}, {
-                                            replies: [
-                                                {
-                                                    type: 'text',
-                                                    content: 'Hmm, I think there\'s a problem with my memory right now. Please try again later.'
-                                                }
-                                            ],
-                                        }, { conversation: { memory: {} } });
-                                        Conversationid.update({ conversationid: received.conversation.id }, { $set: { conversationid: undefined } });
-                                        res.send(toSend);
-                                    } else {
-                                        let toSend = Object.assign({}, {
-                                            replies: [
-                                                {
-                                                    type: 'text',
-                                                    content: 'Alright! Don\'t forget your consultation!'
-                                                }
-                                            ],
-                                        }, { conversation: { memory: {} } });
-                                        Conversationid.update({ conversationid: received.conversation.id }, { $set: { conversationid: undefined } });
-                                        res.send(toSend);
-                                    }
-                                });
-                            } else {
-                                let toSend = Object.assign({}, {
-                                    replies: [
-                                        {
-                                            type: 'text',
-                                            content: 'There seems to be a problem :\\ No worries, just try again later.'
+                            Conversationid.findOne({ conversationid: received.conversation.id }, function (err, obj) {
+                                if (obj) {
+                                    Consultations.create({
+                                        userID: obj.fbid,
+                                        studentID: obj.fbid,
+                                        teacherID: t_id,
+                                        startTime: u_start.format('HH:mm'),
+                                        endTime: u_end.format('HH:mm'),
+                                        date: u_start.dayOfYear(),
+                                        year: u_start.get('year'),
+                                        isDone: false,
+                                        isDeleted: false,
+                                        isApprovedByStudent: true,
+                                        isApprovedByTeacher: false,
+                                        createdAt: new Date()
+                                    }, function (err, res) {
+                                        if (err) {
+                                            let toSend = Object.assign({}, {
+                                                replies: [
+                                                    {
+                                                        type: 'text',
+                                                        content: 'Hmm, I think there\'s a problem with my memory right now. Please try again later.'
+                                                    }
+                                                ],
+                                            }, { conversation: { memory: {} } });
+                                            Conversationid.update({ conversationid: received.conversation.id }, { $set: { conversationid: undefined } });
+                                            res.send(toSend);
+                                        } else {
+                                            let toSend = Object.assign({}, {
+                                                replies: [
+                                                    {
+                                                        type: 'text',
+                                                        content: 'Alright! Don\'t forget your consultation!'
+                                                    }
+                                                ],
+                                            }, { conversation: { memory: {} } });
+                                            Conversationid.update({ conversationid: received.conversation.id }, { $set: { conversationid: undefined } });
+                                            res.send(toSend);
                                         }
-                                    ],
-                                }, { conversation: { memory: {} } });
-                                Conversationid.update({ conversationid: received.conversation.id }, { $set: { conversationid: undefined } });
-                                res.send(toSend);
-                            }
-                        });                        
+                                    });
+                                } else {
+                                    let toSend = Object.assign({}, {
+                                        replies: [
+                                            {
+                                                type: 'text',
+                                                content: 'There seems to be a problem :\\ No worries, just try again later.'
+                                            }
+                                        ],
+                                    }, { conversation: { memory: {} } });
+                                    Conversationid.update({ conversationid: received.conversation.id }, { $set: { conversationid: undefined } });
+                                    res.send(toSend);
+                                }
+                            });
+                        } else {
+                            //error, conflict with other consultations
+                            let toSend = Object.assign({}, {
+                                replies: [
+                                    {
+                                        type: 'text',
+                                        content: 'Looks like your prof\'s in demand. Please schedule in another timeslot.'
+                                    }
+                                ],
+                            }, { conversation: { memory: {} } });
+                            Conversationid.update({ conversationid: received.conversation.id }, { $set: { conversationid: undefined } });
+                            res.send(toSend);
+                            //TODO: SEND CONSULTATION HOURS OF PROFESSOR
+                        }
                     } else {
-                        //error, conflict with other consultations
+                        //error, conflict with consultation hours
                         let toSend = Object.assign({}, {
                             replies: [
                                 {
                                     type: 'text',
-                                    content: 'Looks like your prof\'s in demand. Please schedule in another timeslot.'
+                                    content: 'Remember that you can only schedule a consultation within the professor\'s consultation hours. Please schedule in another timeslot.'
                                 }
                             ],
                         }, { conversation: { memory: {} } });
@@ -580,21 +606,8 @@ app.post("/verify-consultation-hours", (req, res) => {
                         res.send(toSend);
                         //TODO: SEND CONSULTATION HOURS OF PROFESSOR
                     }
-                } else {
-                    //error, conflict with consultation hours
-                    let toSend = Object.assign({}, {
-                        replies: [
-                            {
-                                type: 'text',
-                                content: 'Remember that you can only schedule a consultation within the professor\'s consultation hours. Please schedule in another timeslot.'
-                            }
-                        ],
-                    }, { conversation: { memory: {} } });
-                    Conversationid.update({ conversationid: received.conversation.id }, { $set: { conversationid: undefined } });
-                    res.send(toSend);
-                    //TODO: SEND CONSULTATION HOURS OF PROFESSOR
-                }
-            });
+                });
+            }
         }
     });
 })
