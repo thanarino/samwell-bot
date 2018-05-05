d13fb6c13cbafcce114de30f84833347ae81ce9d
Jonathan Arino <arinojonathan@gmail.com>
2018-05-05 09:06:49 +0800

    sum smol buggfics

--- index.js
+++ index.js
@@ -343,11 +343,12 @@ app.post("/check-available", (req, res) => {
                     console.log(studentID);
                     docs.map((teacher) => {
                         Section.find({ studentList: studentID, teacherList: teacher._id, isDeleted: false }, (err2, docs2) => {
-                            console.log(docs2);
                             if (docs2.length > 0) {
                                 results.push(teacher);
+                                console.log(results);
                             }
                         });
+                        console.log(results);
                     });
                     if (results.length === 0) {
                         //the student is not a student of the professor
