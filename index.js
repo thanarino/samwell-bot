7762774ccb204a5f477cd70048065009ae00b3f6
Jonathan Arino <arinojonathan@gmail.com>
2018-05-06 17:15:19 +0800

    add console.log

--- index.js
+++ index.js
@@ -838,6 +838,7 @@ app.post('/all-consultations', (req, res) => {
             let studentid = obj.fbid;
             console.log(studentid);
             Consultations.find({ studentID: studentid, isDone: false, isApprovedByStudent: true, isApprovedByTeacher: true }).sort({ 'startDate.$date': -1 }).limit(5).exec((err2, consultations) => {
+                console.log(consultations);
                 if (consultations.length > 0) {
                     consultations.map((consultation) => {
                         let c_date = moment().dayOfYear(consultation.date).set({ 'year': consultation.year });
