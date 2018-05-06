98c86259abc5341dc948f7dd990eee024e0bdbc2
Jonathan Arino <arinojonathan@gmail.com>
2018-05-06 17:05:56 +0800

    include new intent in analyzeentities

--- index.js
+++ index.js
@@ -1481,7 +1481,7 @@ analyzeEntities = (sender, res, input) => {
                         }
                     });
                 }
-            } else if (res.intents[0].slug === "confirmentry" || res.intents[0].slug === "getcode" || res.intents[0].slug === "verifycode" || res.intents[0].slug === 'checkavailable' || res.intents[0].slug === 'seeavailable' || res.intents[0].slug === 'nextconsultation') {
+            } else if (res.intents[0].slug === "confirmentry" || res.intents[0].slug === "getcode" || res.intents[0].slug === "verifycode" || res.intents[0].slug === 'checkavailable' || res.intents[0].slug === 'seeavailable' || res.intents[0].slug === 'nextconsultation' || res.intents[0].slug === 'allconsultations') {
                 // conversationId = (typeof conversationId === 'undefined') ? Math.floor((Math.random() * 1000000) + 1) : conversationId;
                 build.dialog({
                         type: 'text',
