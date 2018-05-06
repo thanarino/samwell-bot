ba1fa7e38c64faf9060e15b32b769058857faf41
Jonathan Arino <arinojonathan@gmail.com>
2018-05-06 15:51:16 +0800

    small bugfix

--- index.js
+++ index.js
@@ -899,7 +899,7 @@ app.post('/next-consultation', (req, res) => {
         if (obj) {
             let studentid = obj.fbid;
             console.log(studentid);
-            Consultations.findOne({ studentID: studentid },{ sort: { 'startDate' : -1 } }, (err2, consultation) => {
+            Consultations.findOne({ studentID: studentid },{ sort: { 'startDate.$date' : -1 } }, (err2, consultation) => {
                 console.log(consultation);
                 if (consultation) {
                     Teachers.findOne({ _id: consultation.teacherID }, (err3, teacher) => {
